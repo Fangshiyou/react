7622dbfb0a7c1b851af6c16ac85d4b00430eceed
房世有 <33642994+Fangshiyou@users.noreply.github.com>
2019-10-14 15:42:01 +0800

    Add files via upload

---
+++ build/precache-manifest.4131f499279421a2109f83ce5f6eb87f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d83855107d87c370172100d548619d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6575ac5bb96c67791534",
+    "url": "/static/css/main.720dfbf3.chunk.css"
+  },
+  {
+    "revision": "182ad3db5789afb44447",
+    "url": "/static/js/2.b34c0235.chunk.js"
+  },
+  {
+    "revision": "6575ac5bb96c67791534",
+    "url": "/static/js/main.7b33db4d.chunk.js"
+  },
+  {
+    "revision": "628ea25a63cbc5b89a27",
+    "url": "/static/js/runtime-main.9f147cbc.js"
+  }
+]);
